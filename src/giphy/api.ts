3a0d7a9df28b386ef6cbd4ee434a900689a7e273
Shubham Chaturvedi <19shubham11@gmail.com>
2021-09-29 22:57:07 +0200

    fix: format

--- src/giphy/api.ts
+++ src/giphy/api.ts
@@ -6,16 +6,13 @@ export async function getRandomGIF(tag: string): Promise<string> | never {
     const query = new URLSearchParams({
         api_key: config.giphy.apiKey,
         rating: 'pg',
+        tag,
     })
-
-    if (tag) {
-        query.append('tag', tag)
-    }
 
     try {
         const resp = await GET(`${config.giphy.baseURL}${config.giphy.randomGIFPath}`, query)
-
         const giphyRes = resp.body as GiphyResponse
+
         return giphyRes.data.image_original_url
     } catch (err) {
         throw new Error('giphy error')
