217e1dcca69c60534b9f3e8a981efdac78ca20cf
Shubham Chaturvedi <19shubham11@gmail.com>
2021-09-29 22:02:53 +0200

    feat: add netlify function

---
+++ src/functions/funny.ts
@@ -0,0 +1,16 @@
+import { Handler } from '@netlify/functions'
+import * as giphyAPI from '../giphy/api'
+
+const handler: Handler = async (event, _) => {
+    try {
+        const url = await giphyAPI.getRandomGIF('funny')
+        return {
+            statusCode: 200,
+            body: `<html><body><img src='${url}' c/></body></html>`,
+        }
+    } catch (err) {
+        return { statusCode: 500 }
+    }
+}
+
+export { handler }
